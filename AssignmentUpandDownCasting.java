4c7166805c1c3250725dda7dee82da75a72539eb
Sanjia <amreenishita@gmail.com>
2024-11-17 10:27:49 -0500

    typecasting

---
+++ AssignmentUpandDownCasting.java
@@ -0,0 +1,46 @@
+package TypeCasting;
+
+class ParentClass{
+	static void add(){
+		
+	}
+	static void add1() {
+		
+	}
+    void add2() {
+		
+	}
+    void add4() {
+	
+}
+}
+
+public class AssignmentUpandDownCasting extends ParentClass{
+   static void sub(){
+		
+	}
+	static void sub1() {
+		
+	}
+    void sub2() {
+		
+	}
+    void sub4() {
+	
+}
+	
+
+	public static void main(String[] args) {
+		//UPcasting implicit way 
+		ParentClass n1 = new AssignmentUpandDownCasting();
+		n1.add2();
+		//UPcasting explicit way
+		ParentClass n2 = (ParentClass)new AssignmentUpandDownCasting();
+        n2.add();
+        
+        //down casting done only by explicit way 
+        AssignmentUpandDownCasting g0 = (AssignmentUpandDownCasting)new ParentClass();
+        g0.sub2();
+	}
+
+}
